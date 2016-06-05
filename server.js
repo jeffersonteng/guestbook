f8393ce310ecafd222a5abc538a40497dda07161
Jefferson Teng <jteng3@gmail.com>
2016-06-05 14:06:23 -0700

    Start server on successful mLab connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
 
 // Extract data from form element and adds them onto the body
 // property of the request object. Note: This must be before CRUD handlers.
@@ -20,6 +21,16 @@ app.post('/quotes', (req, res) => {
   console.log(req.body);
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening');
+var url = 'mongodb://' + process.env.MLAB_DB_USER + ':' + process.env.MLAB_DB_PASS + '@ds023603.mlab.com:23603/jteng-todo-app';
+
+var db;
+
+MongoClient.connect(url, (err, database) => {
+  if (err) {
+    return console.log(err);
+  }
+  db = database;
+  app.listen(3000, function() {
+    console.log('Example app listening');
+  });
 });
