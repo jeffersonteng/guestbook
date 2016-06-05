6546509f75ad245950311d1bd4b9e11ee7cb5850
Jefferson Teng <jteng3@gmail.com>
2016-06-05 13:05:54 -0700

    Return index.html on a GET request to /

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000, function() {
