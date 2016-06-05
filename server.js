59d57daa64181663943ca08ee02df9e91e88f07f
Jefferson Teng <jteng3@gmail.com>
2016-06-05 13:52:53 -0700

    Configure dotenv to store mLab database variables

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+require('dotenv').config();
 
 // Extract data from form element and adds them onto the body
 // property of the request object. Note: This must be before CRUD handlers.
@@ -8,6 +9,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/db', (req, res) => {
+  console.log(process.env);
+  res.send('Nice try buddy');
 });
 
 app.post('/quotes', (req, res) => {
