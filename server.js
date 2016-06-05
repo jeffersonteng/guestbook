da0bdfa6e0d9cecf73818f26ff35b61bc6337069
Jefferson Teng <jteng3@gmail.com>
2016-06-05 13:25:32 -0700

    Print body-parser form input on /quotes

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+// Extract data from form element and adds them onto the body
+// property of the request object. Note: This must be before CRUD handlers.
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/quotes', (req, res) => {
-  console.log("Hits /quotes endpoint");
+  console.log(req.body);
 });
 
 app.listen(3000, function() {
