60145a1d798925d5ca8ec0c89ce103edb3a1aeee
Jefferson Teng <jteng3@gmail.com>
2016-06-05 13:18:10 -0700

    Create form that calls /quotes on submit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/quotes', (req, res) => {
+  console.log("Hits /quotes endpoint");
+});
+
 app.listen(3000, function() {
   console.log('Example app listening');
 });
